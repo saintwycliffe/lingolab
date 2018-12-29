812e270dfebc390be9bb6732d3af5ea8e70fadfd
Elijah Rivera <eerivera@mit.edu>
2018-12-28 18:08:40 -0600

    Maven is working

---
+++ lingolab-server/src/main/java/urbana18/Server.java
@@ -0,0 +1,13 @@
+package urbana18;
+
+/**
+ * Hello world!
+ *
+ */
+public class Server
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
