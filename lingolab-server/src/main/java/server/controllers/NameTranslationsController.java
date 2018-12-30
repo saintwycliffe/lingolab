3b680f72091b247b88a8da7c69271f74862f06ba
Elijah Rivera <eerivera@mit.edu>
2018-12-30 15:42:27 -0600

    Rate limiting to 12, to make it work

--- lingolab-server/src/main/java/server/controllers/NameTranslationsController.java
+++ lingolab-server/src/main/java/server/controllers/NameTranslationsController.java
@@ -10,26 +10,21 @@ import java.io.*;
 
 @RestController
 public class NameTranslationsController {
-  private final String API_KEY;
   private final Translate translate;
-  private final List<Language> supportedLanguages;
 
   NameTranslationsController() {
     String tempApiKey = "";
     Translate tempTranslate = null;
-    List<Language> tempSupportedLanguages = new ArrayList<Language>();
     try {
       tempApiKey = this.getApiKey();
       tempTranslate = TranslateOptions.newBuilder().setApiKey(tempApiKey).build().getService();
-      tempSupportedLanguages = tempTranslate.listSupportedLanguages();
     }
     catch (IOException e) {
       System.out.println(e);
     }
     finally {
       this.API_KEY = tempApiKey;
-      this.translate = (tempTranslate != null) ? TranslateOptions.newBuilder().build().getService() : tempTranslate;
-      this.supportedLanguages = tempSupportedLanguages;
+      this.translate = (tempTranslate != null) ? tempTranslate : TranslateOptions.newBuilder().build().getService();
     }
   }
 
@@ -50,6 +45,6 @@ public class NameTranslationsController {
 
   @RequestMapping("/translate")
   public NameTranslations translate(@RequestParam(value="name") String name, @RequestParam(value="lang") String lang_code) {
-    return new NameTranslations(name, lang_code, this.supportedLanguages);
+    return new NameTranslations(name, lang_code, this.translate);
   }
 }
